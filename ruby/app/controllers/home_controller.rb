873f758718a4690b30ce5723b0b6890b3d915ef9
Samir Khoja <khoja@amazon.com>
2015-02-04 16:11:31 -0800

    ruby - adjust parameter names

--- ruby/app/controllers/home_controller.rb
+++ ruby/app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
   # = Pay with Amazon Express Payments Demo
   # The signature method sets the required and optional parameter values and
   # initializes the Express Amazon Payments client. The method is
-  # rendered in json format. A view is not needed for this method. 
+  # rendered in json format. A view is not needed for this method.
   def signature
 
     require 'express_amazon_payments.rb'
@@ -15,13 +15,13 @@ class HomeController < ApplicationController
     access_key = Rails.application.secrets.access_key
     secret_key = Rails.application.secrets.secret_key
     seller_id = Rails.application.secrets.seller_id
-    amount = params[:payment_amount].to_s
+    amount = params[:amount].to_s
     return_url = Rails.application.secrets.return_url
     lwa_client_id = Rails.application.secrets.lwa_client_id
 
     # Optional fields
     # fields are defined in config/secrets.yml
-    currency_code = params[:payment_currency].to_s
+    currency_code = params[:currency_code].to_s
     seller_note = params[:seller_note].to_s
     seller_order_id = Rails.application.secrets.seller_order_id
     shipping_address_required = Rails.application.secrets.shipping_address_required.to_s
